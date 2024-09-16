6d8bf28bf321d369acd9b29e882d619ff3a218f3
wangfiox <wangfiox@gmail.com>
2024-09-17 06:48:28 +0800

    :wrench: + :teda: 8086 的 C 语言环境

--- flake.nix
+++ flake.nix
@@ -12,6 +12,15 @@
         pkgs = import nixpkgs { inherit system; };
         libPath = pkgs.lib.makeLibraryPath [ ]; # 外部库用在 nix 环境中
       in {
-        devShell = pkgs.mkShell { buildInputs = with pkgs; [ nasm dosbox-x ]; };
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [ nasm dosbox-x open-watcom-v2 ];
+          OWTOOLS = "CLANG";
+          OWDOSBOX = "dosbox-x";
+          WATCOM = "${pkgs.open-watcom-v2}";
+          PATH = "$PATH:$WATCOM/armo64";
+          EDDAT = "$WATCOM/eddat";
+          INCLUDE = "$WATCOM/h";
+          LIBS = "$WATCOM/lib386/:$WATCOM/lib386/dos/";
+        };
       });
 }
